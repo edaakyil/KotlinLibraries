b3da02a68bbfc2b75485dcb38493ed82b232e89c
edaakyil <edaakyil@gmail.com>
2025-01-17 04:31:39 +0300

    update

--- KotlinMathLib/src/main/kotlin/com/edaakyil/kotlin/math/MutableComplex.kt
+++ KotlinMathLib/src/main/kotlin/com/edaakyil/kotlin/math/MutableComplex.kt
@@ -5,7 +5,7 @@ import kotlin.math.sqrt
 
 data class MutableComplex(var real: Double = 0.0, var imaginary: Double = 0.0) {
     val norm: Double
-        get() = sqrt((real * real + imaginary * imaginary))
+        get() = sqrt(real * real + imaginary * imaginary)
     val length: Double
         get() = norm
     val conjugate: MutableComplex
